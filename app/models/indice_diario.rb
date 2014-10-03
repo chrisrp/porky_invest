378a6a02b96c59612301972f5ec223775d95be16
Christiano Rodrigues <chrisrp@inventati.org>
2014-10-03 00:35:16 -0300

    adicionando testes de chupinhar

--- app/models/indice_diario.rb
+++ app/models/indice_diario.rb
@@ -6,7 +6,7 @@ class IndiceDiario < ActiveRecord::Base
   self.table_name = 'indices_diarios'
 
   validates :data, presence: true
-  validates :taxa_di, presence: true
+  validate :taxa_di_or_taxa_selic
 
   def self.carregar_ultimo_indice_cdi
       c = ChupinhadorCdi.new
@@ -20,6 +20,30 @@ class IndiceDiario < ActiveRecord::Base
     end
 
     indice
+  end
+
+  def self.carregar_ultima_taxa_selic
+    c = ChupinhadorSelic.new
+    c.chupinhar
+
+    taxa = IndiceDiario.find_by(data: c.data)
+
+    if taxa.nil?
+      taxa = IndiceDiario.new(data: c.data, taxa_selic: c.taxa)
+      taxa.save
+    elsif taxa.taxa_selic.nil?
+      taxa.update_attributes(taxa_selic: c.taxa)
+    end
+
+    taxa
+  end
+
+  private
+
+  def taxa_di_or_taxa_selic
+    unless taxa_di.present? || taxa_selic.present?
+      errors.add(:base, 'preencher taxa selic ou di')
+    end
   end
 
 end
--- spec/models/indice_diario_spec.rb
+++ spec/models/indice_diario_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 describe IndiceDiario do
   let(:data) { '01/01/2014' }
   let(:taxa_di) { 10.8 }
+  let(:taxa_selic) { 10.9 }
 
-  let(:indice) { { data: data, taxa_di: taxa_di }  }
+  let(:indice) { { data: data, taxa_di: taxa_di, taxa_selic: taxa_selic }  }
 
   before { allow_any_instance_of(ChupinhadorCdi).to receive(:chupinhar).and_return('') }
 
@@ -20,17 +21,32 @@ describe IndiceDiario do
       it { expect(subject).not_to be_valid }
     end
 
-    context 'sem taxa' do
+    context 'sem taxa di e sem taxa selic' do
       let(:taxa_di) { nil }
+      let(:taxa_selic) { nil }
 
       it { expect(subject).not_to be_valid }
     end
-  end
+
+    context 'com taxa di e sem taxa selic' do
+      let(:taxa_di) { 10.5 }
+      let(:taxa_selic) { nil }
+
+      it { expect(subject).to be_valid }
+    end
+
+    context 'com taxa di e sem taxa selic' do
+      let(:taxa_di) { nil }
+      let(:taxa_selic) { 10.0 }
+
+      it { expect(subject).to be_valid }
+    end
+    end
 
   context 'com data pt-br' do
     let(:data) { '31/12/2014' }
 
-    it { expect(subject.data).to eql Date.new(2014,12,31) }
+    it { expect(subject.data).to eql Date.new(2014, 12, 31) }
   end
 
   describe '#carregar_ultimo_indice_cdi' do
@@ -62,6 +78,52 @@ describe IndiceDiario do
       it { expect(subject.data).to eql ultimo_indice.data }
       it { expect(subject.taxa_di).to eql ultimo_indice.taxa_di }
     end
-
   end
+
+  describe '#carregar_ultima_taxa_selic' do
+    context 'quando já existe taxa do dia' do
+      let(:taxa_do_dia) do
+        FactoryGirl.create(:indice_diario,
+                           data: Date.today,
+                           taxa_selic: 10.5,
+                           taxa_di: nil)
+      end
+
+      before do
+        taxa_do_dia
+        allow_any_instance_of(ChupinhadorSelic).to receive(:chupinhar)
+        allow_any_instance_of(ChupinhadorSelic).to receive(:data).and_return(Date.today)
+      end
+
+      subject { IndiceDiario.carregar_ultima_taxa_selic }
+
+      it { expect(subject).to eql taxa_do_dia }
+    end
+
+    context 'quando não existe taxa selic cadastrada' do
+      before do
+        allow_any_instance_of(ChupinhadorSelic).to receive(:data).and_return(Date.today)
+        allow_any_instance_of(ChupinhadorSelic).to receive(:taxa).and_return(10.4)
+      end
+
+      subject { IndiceDiario.carregar_ultima_taxa_selic }
+
+      it { expect(subject.data).to eql Date.today }
+      it { expect(subject.taxa_selic).to eql 10.4 }
+    end
+
+    context 'quando existe taxa di mas não selic' do
+      before do
+        FactoryGirl.create(:indice_diario, data: Date.today)
+        allow_any_instance_of(ChupinhadorSelic).to receive(:taxa).and_return(10.2)
+        allow_any_instance_of(ChupinhadorSelic).to receive(:data).and_return(Date.today)
+      end
+
+      subject { IndiceDiario.carregar_ultima_taxa_selic }
+
+      it { expect(subject.data).to eql Date.today }
+      it { expect(subject.taxa_selic).to eql 10.2 }
+    end
+  end
+
 end
